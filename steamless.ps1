c8c2b20f5925a750354929025479166a6edaf7dd
Juan Domingo Peron <94873456+Peron4TheWin@users.noreply.github.com>
2026-06-09 15:55:17 -0300

    Refactor steamless.ps1 for improved readability

--- steamless.ps1
+++ steamless.ps1
@@ -1,7 +1,7 @@
 param($ExePath)
-$tmp = "$env:TEMP\steamless"
-$zip = "$tmp\steamless.zip"
-New-Item -ItemType Directory -Force -Path $tmp | Out-Null
-Invoke-WebRequest "https://github.com/atom0s/Steamless/releases/download/v3.1.0.5/Steamless.v3.1.0.5.-.by.atom0s.zip" -OutFile $zip
-Expand-Archive -Path $zip -DestinationPath $tmp -Force
+$tmp="$env:TEMP\steamless"
+New-Item -ItemType Directory -Force -Path $tmp|Out-Null
+irm "https://github.com/atom0s/Steamless/releases/download/v3.1.0.5/Steamless.v3.1.0.5.-.by.atom0s.zip" -OutFile "$tmp\s.zip"
+Expand-Archive "$tmp\s.zip" $tmp -Force
 & "$tmp\Steamless.CLI.exe" $ExePath
+if(Test-Path "$ExePath.unpacked.exe"){Remove-Item $ExePath -Force;Rename-Item "$ExePath.unpacked.exe" $ExePath}
